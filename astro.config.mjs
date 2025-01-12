9c6750ab513064b63d79373913c681f6a2000a74
DLeonardoG <darkleonardog@gmail.com>
2025-01-12 13:59:28 -0500

    feat: :sparkles: successfully added language
    it took us longer than expected but it was accomplished
    
    By @DLeonardoG

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
-import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
+import react from '@astrojs/react';
+
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'es'],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  },
+
+  integrations: [tailwind(), react()]
 });
